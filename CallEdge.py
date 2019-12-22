707c470cfbdeb78db56eed309729735ef7b708a3
mhdr-hami <mhdr.hami@gmail.com>
2019-12-23 00:02:25 +0330

    calls added

--- CallEdge.py
+++ CallEdge.py
@@ -2,7 +2,7 @@ from Edge import *
 
 
 class CallEdge(Edge):
-    def __init__(self, FromNode, ToNode, CallId, Date, Duration):
+    def __init__(self, CallId, FromNode, ToNode, Date, Duration):
         self.FromNode = FromNode
         self.ToNode = ToNode
         self.UniqueKey = CallId
