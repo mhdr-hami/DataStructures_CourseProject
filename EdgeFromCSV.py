d8eca6c7ca9ba3a12511d3602db4968710c4656e
mhdr-hami <mhdr.hami@gmail.com>
2019-12-23 00:41:15 +0330

    calls columns fixed

--- EdgeFromCSV.py
+++ EdgeFromCSV.py
@@ -16,11 +16,10 @@ for i in range(len(df_ownerships)):
 
 #Calls
 df_calls = pd.read_csv('data/calls.csv')
-df_calls = df_calls[[df_calls.columns[2], df_calls.columns[0], df_calls.columns[1], df_calls.columns[3], df_calls.columns[4]]]
 callsDictionary = {}
 for i in range(len(df_calls)):
-    call_tmp = CallEdge(df_calls.iloc[i][0], df_calls.iloc[i][1], df_calls.iloc[i][2], df_calls.iloc[i][3], df_calls.iloc[i][4])
-    callsDictionary[df_calls.iloc[i][0]] = call_tmp
+    call_tmp = CallEdge(df_calls.iloc[i][2], df_calls.iloc[i][0], df_calls.iloc[i][1], df_calls.iloc[i][3], df_calls.iloc[i][4])
+    callsDictionary[df_calls.iloc[i][2]] = call_tmp
 
 ########################################################################################################################
 
