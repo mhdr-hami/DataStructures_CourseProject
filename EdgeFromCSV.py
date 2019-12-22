a4565a8aeb6368e01ac2b6636a20667459e0f644
mhdr-hami <mhdr.hami@gmail.com>
2019-12-23 00:39:32 +0330

    ownerships columns fixed

--- EdgeFromCSV.py
+++ EdgeFromCSV.py
@@ -7,11 +7,10 @@ from CallEdge import *
 
 #Ownerships
 df_ownerships = pd.read_csv('data/ownerships.csv')
-df_ownerships = df_ownerships[[df_ownerships.columns[2], df_ownerships.columns[0], df_ownerships.columns[1], df_ownerships.columns[3], df_ownerships.columns[4]]]
 ownerShipsDictionary = {}
 for i in range(len(df_ownerships)):
-    ownership_tmp = OwnershipEdge(df_ownerships.iloc[i][0], df_ownerships.iloc[i][1], df_ownerships.iloc[i][2], df_ownerships.iloc[i][3], df_ownerships.iloc[i][4])
-    ownerShipsDictionary[df_ownerships.iloc[i][0]] = ownership_tmp
+    ownership_tmp = OwnershipEdge(df_ownerships.iloc[i][2], df_ownerships.iloc[i][0], df_ownerships.iloc[i][1], df_ownerships.iloc[i][3], df_ownerships.iloc[i][4])
+    ownerShipsDictionary[df_ownerships.iloc[i][2]] = ownership_tmp
 
 ########################################################################################################################
 
