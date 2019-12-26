5792716b61e11c1ef944929919062c1e93a25a81
mhdr-hami <mhdr.hami@gmail.com>
2019-12-26 13:57:07 +0330

    outgoing/incoming objects turned to keys

--- EdgeFromCSV.py
+++ EdgeFromCSV.py
@@ -14,7 +14,8 @@ for i in range(len(df_ownerships)):
     ownership_tmp = OwnershipEdge(df_ownerships.iloc[i][2], df_ownerships.iloc[i][0], df_ownerships.iloc[i][1], df_ownerships.iloc[i][3], df_ownerships.iloc[i][4])
     ownerShipsDictionary[df_ownerships.iloc[i][2]] = ownership_tmp
 for key in ownerShipsDictionary:
-    personDictionary[str(ownerShipsDictionary[key].fromNode)].outgoing.append(ownerShipsDictionary[key])
+    tmp_list = [key, "ownerShip"]
+    personDictionary[str(ownerShipsDictionary[key].fromNode)].outgoing.append(tmp_list)
 
 ########################################################################################################################
 
@@ -25,8 +26,9 @@ for i in range(len(df_calls)):
     call_tmp = CallEdge(df_calls.iloc[i][2], df_calls.iloc[i][0], df_calls.iloc[i][1], df_calls.iloc[i][3], df_calls.iloc[i][4])
     callsDictionary[df_calls.iloc[i][2]] = call_tmp
 for key in callsDictionary:
-    personDictionary[str(phonesDictionary[str(callsDictionary[key].fromNode)].idNumber)].outgoing.append(callsDictionary[key])
-    personDictionary[str(phonesDictionary[str(callsDictionary[key].toNode)].idNumber)].incomming.append(callsDictionary[key])
+    tmp_list = [key, "call"]
+    personDictionary[str(phonesDictionary[str(callsDictionary[key].fromNode)].idNumber)].outgoing.append(tmp_list)
+    personDictionary[str(phonesDictionary[str(callsDictionary[key].toNode)].idNumber)].incomming.append(tmp_list)
 
 ########################################################################################################################
 
@@ -37,8 +39,9 @@ for i in range(len(df_transactions)):
     transaction_tmp = TransactionEdge(df_transactions.iloc[i][2], df_transactions.iloc[i][0], df_transactions.iloc[i][1], df_transactions.iloc[i][3], df_transactions.iloc[i][4])
     transactionsDictionary[str(df_transactions.iloc[i][2])] = transaction_tmp
 for key in transactionsDictionary:
-    personDictionary[str(bankAccDictionary[str(transactionsDictionary[key].fromNode)].idNumber)].outgoing.append(transactionsDictionary[key])
-    personDictionary[str(bankAccDictionary[str(transactionsDictionary[key].toNode)].idNumber)].incomming.append(transactionsDictionary[key])
+    tmp_list = [key, "transaction"]
+    personDictionary[str(bankAccDictionary[str(transactionsDictionary[key].fromNode)].idNumber)].outgoing.append(tmp_list)
+    personDictionary[str(bankAccDictionary[str(transactionsDictionary[key].toNode)].idNumber)].incomming.append(tmp_list)
 
 ########################################################################################################################
 
@@ -50,5 +53,9 @@ for i in range(len(df_relations)):
     relation_tmp = RelationEdge(x, df_relations.iloc[i][0], df_relations.iloc[i][1], df_relations.iloc[i][2], df_relations.iloc[i][3])
     relationsDictionary[x] = relation_tmp
 for key in relationsDictionary:
-    personDictionary[str(relationsDictionary[key].fromNode)].outgoing.append(relationsDictionary[key])
-    personDictionary[str(relationsDictionary[key].toNode)].incomming.append(relationsDictionary[key])
+    tmp_list = [key, "relation"]
+    personDictionary[str(relationsDictionary[key].fromNode)].outgoing.append(tmp_list)
+    personDictionary[str(relationsDictionary[key].toNode)].incomming.append(tmp_list)
+
+########################################################################################################################
+
