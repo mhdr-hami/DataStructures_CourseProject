fbda393936ffce84a78e527e0240a5550613672e
mhdr-hami <mhdr.hami@gmail.com>
2019-12-27 18:39:13 +0330

    faze two transported

--- EdgeFromCSV.py
+++ EdgeFromCSV.py
@@ -58,32 +58,3 @@ for key in relationsDictionary:
     personDictionary[str(relationsDictionary[key].toNode)].incomming.append(tmp_list)
 
 ########################################################################################################################
-
-#faze2
-for key in personDictionary:
-    if personDictionary[key].job == "گمرک":
-        print(personDictionary[key].unique_Key)
-for key in personDictionary:
-    suspected = {}
-    if personDictionary[key].job == "گمرک":
-        checkingNodes = []
-        for item in personDictionary[key].outgoing:
-            if item[1] == "relation":
-                checkingNodes.append(relationsDictionary[str(item[0])].toNode)
-                suspected[relationsDictionary[str(item[0])].toNode] = 1
-        maxDist = 1
-        while maxDist <= 5 and len(checkingNodes) != 0:
-            topNodeKey = checkingNodes[0]
-            topNode = personDictionary[str(topNodeKey)]
-            checkingNodes.pop()
-            for item in topNode.outgoing:
-                if item[1] == "relation" and not relationsDictionary[str(item[0])].fromNode in suspected:
-                    checkingNodes.append(relationsDictionary[str(item[0])].toNode)
-                    suspected[relationsDictionary[str(item[0])].toNode] = suspected[topNodeKey] + 1
-                    maxDist = suspected[topNodeKey] + 1
-    for sPerson in suspected:
-        for key2 in personDictionary[str(sPerson)].outgoing:
-            if key2[1] == "ownerShip":
-                x = list(str(ownerShipsDictionary[key2[0]].buyDate).split("-"))
-                if ownerShipsDictionary[key2[0]].fromNode == personDictionary[str(sPerson)].unique_Key and 2020 * 365 - int(x[0]) * 365 + int(x[1]) * 30 + int(x[2]) < 365 * 2:
-                    print(personDictionary[key].unique_Key, personDictionary[key].job, ownerShipsDictionary[key2[0]].buyDate, ownerShipsDictionary[key2[0]].fromNode)
