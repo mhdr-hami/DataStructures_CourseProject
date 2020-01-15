9a2130174c7e6948989968cffd315733dc8bd8fe
safdarian <safdarian2000@gmail.com>
2020-01-16 00:18:25 +0330

    localhost Address Changed to 127.1.1.0

--- Main.py
+++ Main.py
@@ -1,17 +1,16 @@
 import os
 from flask import render_template, request, redirect, url_for
-from werkzeug import secure_filename
+# from werkzeug import secure_filename
 from flask import Flask
 from flask import request
-import runpy
 
 app = Flask(__name__)
-
 
 
 @app.route('/')
 def index():
     return render_template("FileReader.html")
+
 
 @app.route('/Loading', methods=['GET', 'POST'])
 def upload():
@@ -29,8 +28,8 @@ def upload():
         # save each "charts" file
         for file in request.files.getlist('TheDataset'):
             # file.save(os.path.join(uploads_dir, file.name))
-            print(os.path.join(uploads_dir, secure_filename(file.filename)))
-            file.save(os.path.join(uploads_dir, secure_filename(file.filename)))
+            print(os.path.join(uploads_dir, (file.filename)))
+            file.save(os.path.join(uploads_dir, (file.filename)))
 
         # return redirect(url_for('upload'))
 
@@ -42,4 +41,5 @@ def upload():
 
     return render_template('FileReader.html')
 
-app.run(debug=True, port=8000, host='0.0.0.0')
+
+app.run(debug=True, port=8000, host='127.1.1.0')
